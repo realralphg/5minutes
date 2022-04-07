d3dd1201efe2e82ed9f05a27587b1bfd1613a9c4
Daniel <dondaniel028@example.com>
2022-04-07 11:38:43 +0100

    Done with login and register pages.

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,22 +4,24 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '/', component: () => import('pages/Index.vue') },
+      {
+        path: '/',
+        component: () => import('pages/Index.vue'),
+        children: [
+          {
+            path: '/',
+            component: ()=>import('pages/user/Home.vue')
+          }
+        ]
+      },
       { path: '/login', component: () => import('pages/Login.vue') },
-      { path: '/register', component: () => import('pages/Register.vue') },
+      { path: '/register', component: () => import('src/pages/Register.vue') },
       { path: '/chat', component: () => import('pages/Chat.vue') },
       { path: '/profile', component: () => import('pages/Profile.vue') },
       { path: '/mentor', component: () => import('pages/Mentor.vue') },
       { path: '/onboard', component: () => import('pages/Onboard.vue') },
       { path: '/onboardg', component: () => import('pages/Onboardg.vue') },
       { path: '/onboardgr', component: () => import('pages/Onboardgr.vue') },
-
-
-
-
-
-
-
 
     ]
   },
